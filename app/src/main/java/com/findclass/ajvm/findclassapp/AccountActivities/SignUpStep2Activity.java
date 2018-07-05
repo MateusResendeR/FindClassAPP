544abc41123a8447c255b2f2446f3c8357b3876c
Adelino Neto <adelinon7a@gmail.com>
2018-07-05 18:45:30 -0300

    Atualização visual e de controle do Cadastro com E-mail

--- app/src/main/java/com/findclass/ajvm/findclassapp/AccountActivities/SignUpStep2Activity.java
+++ app/src/main/java/com/findclass/ajvm/findclassapp/AccountActivities/SignUpStep2Activity.java
@@ -1,7 +1,6 @@
 package com.findclass.ajvm.findclassapp.AccountActivities;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -17,8 +16,6 @@ import com.findclass.ajvm.findclassapp.Exception.PhoneLenghtException;
 import com.findclass.ajvm.findclassapp.MainActivity;
 import com.findclass.ajvm.findclassapp.Model.User;
 import com.findclass.ajvm.findclassapp.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -26,23 +23,29 @@ import com.google.firebase.database.FirebaseDatabase;
 public class SignUpStep2Activity extends AppCompatActivity {
     private FirebaseAuth auth;
     private DatabaseReference db;
+    private String uid;
+    private String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         auth = FirebaseAuth.getInstance();
         db = FirebaseDatabase.getInstance().getReference();
 
+        uid = auth.getCurrentUser().getUid().toString();
+        email = auth.getCurrentUser().getEmail().toString();
+        auth.signOut();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_step2);
     }
 
     public void finishSignUp(View view) {
-        EditText name = (EditText) findViewById(R.id.nameEditText);
-        EditText surname = (EditText) findViewById(R.id.surnameEditText);
-        EditText cpf = (EditText) findViewById(R.id.cpfEditText);
-        EditText birthdate = (EditText) findViewById(R.id.birthdateEditText);
-        EditText phone = (EditText) findViewById(R.id.phoneEditText);
-        CheckBox professor = (CheckBox) findViewById(R.id.professorCheckBox);
+        EditText name = findViewById(R.id.nameEditText);
+        EditText surname = findViewById(R.id.surnameEditText);
+        EditText cpf = findViewById(R.id.cpfEditText);
+        EditText birthdate = findViewById(R.id.birthdateEditText);
+        EditText phone = findViewById(R.id.phoneEditText);
+        CheckBox professor = findViewById(R.id.professorCheckBox);
 
         try {
             if (thereAreEmptyFields(name, surname, cpf, birthdate, phone)) {
@@ -54,24 +57,15 @@ public class SignUpStep2Activity extends AppCompatActivity {
             } else if (phone.getText().length() < 14) {
                 throw new PhoneLenghtException();
             } else {
-                User user = new User(auth.getCurrentUser().getEmail(), name.getText().toString(),
+                User user = new User(email, name.getText().toString(),
                         surname.getText().toString(), cpf.getText().toString(), birthdate.getText().toString(),
                         phone.getText().toString(), professor.isChecked());
 
                 if (db != null) {
-                    db.child("users").child(auth.getCurrentUser().getUid().toString()).setValue(user);
+                    db.child("users").child(uid).setValue(user);
                 }
 
                 Toast.makeText(this, "Cadastro realizado com sucesso.", Toast.LENGTH_SHORT).show();
-
-                auth.getCurrentUser().sendEmailVerification().addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(SignUpStep2Activity.this, "E-mail de confirmação enviado.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
 
                 Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
