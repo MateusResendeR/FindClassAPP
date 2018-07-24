0eaa314c283cc7ae9d6b9474657604e168b4a411
Victor Souto <victorsoouto@gmail.com>
2018-07-24 19:49:48 -0300

    Testes referentes a busca e adição de disciplinas e acrecissomos de testes referentes a outras US's

--- app/src/androidTest/java/com/findclass/ajvm/findclassapp/MenuAlunoActivityTest.java
+++ app/src/androidTest/java/com/findclass/ajvm/findclassapp/MenuAlunoActivityTest.java
@@ -1,15 +1,35 @@
 package com.findclass.ajvm.findclassapp;
 
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
 import com.findclass.ajvm.findclassapp.menuActivities.MenuAlunoActivity;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(AndroidJUnit4.class)
 public class MenuAlunoActivityTest {
@@ -19,8 +39,10 @@ public class MenuAlunoActivityTest {
             mActivityRule = new ActivityTestRule<>(MenuAlunoActivity.class, false, true);
 
     @Test
-    public void whenActivityIsLaunched_shouldDisplayInitialState(){
+    public void whenActivityIsLaunched_shouldDisplayInitialState() throws InterruptedException {
+        Thread.sleep(1500);
         onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
+        onView(withId(R.id.menuPesquisa)).check(matches(isDisplayed()));
     }
 
 }
