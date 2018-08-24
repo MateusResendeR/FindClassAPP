bf906f85913c7ca51a529c8e39eaf3cf0934521b
Mateus <matt.rod@hotmail.com>
2018-08-24 20:47:06 -0300

    [#159961895]Refatoração: Disciplinas dos professores

--- app/src/main/java/com/findclass/ajvm/findclassapp/menuActivities/SubjectOfProfessorActivity.java
+++ app/src/main/java/com/findclass/ajvm/findclassapp/menuActivities/SubjectOfProfessorActivity.java
@@ -2,13 +2,10 @@ package com.findclass.ajvm.findclassapp.menuActivities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.TextView;
@@ -27,17 +24,19 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class SubjectOfProfessorActivity extends AppCompatActivity {
-    private User professor;
-    private TextView textViewNome;
+    //Elemnetos do firebase
     private DatabaseReference professorSubjectRef;
     private DatabaseReference subjectRef;
+    //Elementos gráficos
+    private TextView textViewNome;
     private RecyclerView recyclerViewSubjectsOfProfessor;
+    //Elementos auxiliares
     private SubjectOfProfessorAdapter adapter;
-    private ArrayList<Subject_Professor> listSubjectProfessor = new ArrayList<>();
     private ValueEventListener valueEventListenerSubjectProfessor;
+    private ArrayList<Subject_Professor> listSubjectProfessor = new ArrayList<>();
+    private User professor;
 
 
     @Override
@@ -46,34 +45,33 @@ public class SubjectOfProfessorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_subject_of_professor);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        recyclerViewSubjectsOfProfessor = findViewById(R.id.recyclerViewSubjectsOfProfessor);
-
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //setando atributos
         adapter = new SubjectOfProfessorAdapter(listSubjectProfessor,this);
-
+        //Setando atributos gráficos
+        recyclerViewSubjectsOfProfessor = findViewById(R.id.recyclerViewSubjectsOfProfessor);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerViewSubjectsOfProfessor.setLayoutManager(layoutManager);
         recyclerViewSubjectsOfProfessor.setHasFixedSize(true);
         recyclerViewSubjectsOfProfessor.setAdapter(adapter);
-
         textViewNome = findViewById(R.id.textViewNameProfessorOfSubject);
-
-
+        //Recuperando dados da Activity passada
         Bundle bundle = getIntent().getExtras();
         if(bundle != null) {
             professor = (User) bundle.getSerializable("user");
             textViewNome.setText("Professor: "+professor.getName()+" "+professor.getSurname());
-
         }
+        //setando atributos do firebase
         professorSubjectRef = FirebaseDatabase.getInstance().getReference().child("professorSubjects").child(professor.getId());
         subjectRef = FirebaseDatabase.getInstance().getReference().child("subjects");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Adição do evento de clique aos itens da lista
         recyclerViewSubjectsOfProfessor.addOnItemTouchListener(
                 new RecyclerItemClickListener(
                         this,
                         recyclerViewSubjectsOfProfessor,
                         new RecyclerItemClickListener.OnItemClickListener() {
+                            //Definição da ação do clique.
                             @Override
                             public void onItemClick(View view, int position) {
                                 Intent intent = new Intent(getBaseContext(),
@@ -87,24 +85,24 @@ public class SubjectOfProfessorActivity extends AppCompatActivity {
 
                             @Override
                             public void onLongItemClick(View view, int position) {
-
+                                //
                             }
 
                             @Override
                             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
+                                //
                             }
                         }
                 )
         );
     }
-
+    //Método que define as ações que devem ser executadas ao iniciar a Activity
     @Override
     protected void onStart() {
         super.onStart();
-        getSubjectProfessor();
+        retriveSubjectOfProfessor();
     }
-
+    //Método que define as ações que devem ser executadas ao abandonar a Activity
     @Override
     protected void onStop() {
         super.onStop();
@@ -112,6 +110,7 @@ public class SubjectOfProfessorActivity extends AppCompatActivity {
         professorSubjectRef.removeEventListener(valueEventListenerSubjectProfessor);
     }
 
+    //Método que define a ação do botão voltar do hardware
     @Override
     public void onBackPressed() {
         Intent intent = new Intent(getBaseContext(), MenuAlunoActivity.class);
@@ -119,33 +118,35 @@ public class SubjectOfProfessorActivity extends AppCompatActivity {
         finish();
     }
 
-    public void getSubjectProfessor(){
+    //Método para buscar no banco de dados as disciplinas dos professores
+    public void retriveSubjectOfProfessor(){
         listSubjectProfessor.clear();
         valueEventListenerSubjectProfessor = professorSubjectRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for(DataSnapshot data: dataSnapshot.getChildren()){
-                    final Professor_Subject ps = data.getValue(Professor_Subject.class);
+                    final Professor_Subject professor_subject = data.getValue(Professor_Subject.class);
                     final Subject subject = new Subject();
-                    subjectRef.child(ps.getSubjectId()).addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            subject.setSubject(dataSnapshot.getValue(Subject.class));
-                            Subject_Professor sp = new Subject_Professor(professor,subject,ps);
-                            listSubjectProfessor.add(sp);
-                            adapter.notifyDataSetChanged();
-
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-
-                        }
-                    });
-
+                    retriveSubject(professor_subject,subject);
                 }
             }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
+    }
+
+    //Método para buscar no banco de dados as disciplinas
+    public void retriveSubject(final Professor_Subject professor_subject, final Subject subject){
+        subjectRef.child(professor_subject.getSubjectId()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                subject.setSubject(dataSnapshot.getValue(Subject.class));
+                Subject_Professor sp = new Subject_Professor(professor,subject,professor_subject);
+                listSubjectProfessor.add(sp);
+                adapter.notifyDataSetChanged();
+            }
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
