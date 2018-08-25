464b8e1315e8cf23035ef8e518ac03fa833f95d5
Mateus <matt.rod@hotmail.com>
2018-08-24 21:35:55 -0300

    [#159961895]Refatoração: Ranking de professores

--- app/src/main/java/com/findclass/ajvm/findclassapp/menuActivities/ProfessorRankingActivity.java
+++ app/src/main/java/com/findclass/ajvm/findclassapp/menuActivities/ProfessorRankingActivity.java
@@ -2,8 +2,6 @@ package com.findclass.ajvm.findclassapp.menuActivities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -25,10 +23,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class ProfessorRankingActivity extends AppCompatActivity {
-    private RecyclerView recyclerViewProfessorRanking;
+    //Elemenetos do firebase
+    private DatabaseReference userRef;
+    //Elementos auxiliares
     private ProfessorRankingAdapter adapter;
     private ArrayList<User> listProfessors = new ArrayList<>();
-    private DatabaseReference userRef;
     private ValueEventListener valueEventListenerProfessor;
 
     @Override
@@ -37,19 +36,18 @@ public class ProfessorRankingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_professor_ranking);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        recyclerViewProfessorRanking = findViewById(R.id.recyclerViewProfessorRanking);
-        userRef = FirebaseDatabase.getInstance().getReference().child("users");
-
+        //Setando atributos
         adapter = new ProfessorRankingAdapter(listProfessors,this);
-
+        //setando atributos do firebase
+        userRef = FirebaseDatabase.getInstance().getReference().child("users");
+        //Setando atributos gráficos
+        RecyclerView recyclerViewProfessorRanking = findViewById(R.id.recyclerViewProfessorRanking);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerViewProfessorRanking.setLayoutManager(layoutManager);
         recyclerViewProfessorRanking.setHasFixedSize(true);
         recyclerViewProfessorRanking.setAdapter(adapter);
-
+        //Adição do evento de clique aos itens da lista
         recyclerViewProfessorRanking.addOnItemTouchListener(
                 new RecyclerItemClickListener(
                         this,
@@ -61,35 +59,36 @@ public class ProfessorRankingActivity extends AppCompatActivity {
                                 intent.putExtra("user", listProfessors.get(position));
                                 startActivity(intent);
                             }
-
                             @Override
                             public void onLongItemClick(View view, int position) {
-
+                                //
                             }
 
                             @Override
                             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
+                                //
                             }
                         }
                 )
         );
-
     }
 
+    //Método que define as ações que devem ser executadas ao iniciar a Activity
     @Override
     protected void onStart() {
         super.onStart();
-        getProfessors();
+        retriverProfessors();
     }
 
+    //Método que define as ações que devem ser executadas ao abandonar a Activity
     @Override
     protected void onStop() {
         super.onStop();
         userRef.removeEventListener(valueEventListenerProfessor);
     }
 
-    public void getProfessors(){
+    //Método para buscar no banco de dados todos os professores
+    public void retriverProfessors(){
         listProfessors.clear();
         valueEventListenerProfessor = userRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -100,13 +99,13 @@ public class ProfessorRankingActivity extends AppCompatActivity {
                         listProfessors.add(user);
                     }
                 }
+                //Ordenando lista de professores pelo ranking
                 Collections.sort(listProfessors);
                 adapter.notifyDataSetChanged();
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                //
             }
         });
     }
